153ebbba3be2093f441c65064a396f3650dc7aac
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-03-16 15:43:51 +0100

    sorted pizzas by their popularity

--- src/store/pizzas/selectors.js
+++ src/store/pizzas/selectors.js
@@ -1,4 +1,4 @@
-export const selectNumberOfPizzas = (reduxState) => {
+/*export const selectNumberOfPizzas = (reduxState) => {
   return reduxState.pizzas.allPizzas.length;
 };
 
@@ -10,7 +10,17 @@ export const selectMostBoughtPizza = (reduxState) => {
   return reduxState.pizzas.allPizzas.reduce((mostBought, nextPizza) => {
     return mostBought.bought >= nextPizza.bought ? mostBought : nextPizza;
   });
-};
+};*/
 
 //Define a selector to get the list of pizzas:
-export const selectPizzas = (reduxState) => reduxState.pizzas.allPizzas;
+//export const selectPizzas = (reduxState) => reduxState.pizzas.allPizzas;
+
+//sort the pizzas by their popularity (a.k.a. pizzas bought more often, are more popular)
+/* BE CAREFUL HERE!!!!! The sort() method sorts the elements of an array "in place" and returns the sorted array. For this reason, I need to first CLONE my array -otherwise .sort will edit the state directly!!!*/
+export const selectPizzas = (reduxState) => {
+  const sortedPizzas = [...reduxState.pizzas.allPizzas];
+
+  return sortedPizzas.sort((a, b) => {
+    return b.bought - a.bought;
+  });
+};
