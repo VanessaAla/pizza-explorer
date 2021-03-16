c1e322ea16c39218965d1b5352bf15573ca2393c
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-03-16 17:38:52 +0100

    added a switch case to the user's reducer

--- src/store/user/reducer.js
+++ src/store/user/reducer.js
@@ -6,6 +6,20 @@ const initialState = {
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
+    //Add a switch case to the user reducer that deals with these "user/toggleFavorite" type actions, and toggles whether the pizza in question is a favorite or not.
+    case "user/toggleFavorite": {
+      const id = action.payload;
+
+      const favorites = state.favorites.includes(id) // check if we already have it
+        ? state.favorites.filter((f) => f !== id) // remove
+        : [...state.favorites, id]; // add
+
+      return {
+        ...state,
+        favorites,
+      };
+    }
+
     default: {
       return state;
     }
