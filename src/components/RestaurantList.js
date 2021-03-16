057ecd82cd25990dcd159801dee6af6ffff431da
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-03-16 19:19:13 +0100

    replaced the restaurant list with a new dropdown

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,32 +1,54 @@
 import { useSelector } from "react-redux";
 //import { selectRestaurants } from "../store/restaurants/selectors";
-import { selectRestaurantsWithPizzas } from "../store/selectors";
+//import { selectRestaurantsWithPizzas } from "../store/selectors";
+import { selectPizzasSoldByRestaurant } from "../store/selectors";
 import { selectPizzas } from "../store/pizzas/selectors";
 import { useState } from "react";
-import { selectRestaurantsThatSellPizza } from "../store/restaurants/selectors";
+//import { selectRestaurantsThatSellPizza } from "../store/restaurants/selectors";
+import {
+  selectRestaurants,
+  selectRestaurantsThatSellPizza,
+} from "../store/restaurants/selectors";
 
 export default function RestaurantList() {
-  //const restaurants = useSelector(selectRestaurants);
-  const restaurants = useSelector(selectRestaurantsWithPizzas);
+  const restaurants = useSelector(selectRestaurants);
+  //const restaurants = useSelector(selectRestaurantsWithPizzas);
   const pizzas = useSelector(selectPizzas);
   const [selectedPizza, setSelectedPizza] = useState(pizzas[0].id);
+  const [selectedRestaurant, setSelectedRestaurant] = useState(
+    restaurants[0].id
+  );
+
   const restaurantsThatSellSelectedPizza = useSelector(
     selectRestaurantsThatSellPizza(selectedPizza)
+  );
+
+  const pizzasSoldBySelectedRestaurant = useSelector(
+    selectPizzasSoldByRestaurant(selectedRestaurant)
   );
 
   return (
     <div>
       <h1>Restaurants</h1>
       <ul>
-        {restaurants.map((restaurant) => (
-          <li key={restaurant.id}>
-            {restaurant.name}
-            <ul>
-              {restaurant.pizzas.map((pizza) => (
-                <li key={pizza.id}>{pizza.name}</li>
-              ))}
-            </ul>
-          </li>
+        <h2>
+          What does{" "}
+          <select
+            value={selectedRestaurant}
+            onChange={(e) => {
+              setSelectedRestaurant(parseInt(e.target.value));
+            }}
+          >
+            {restaurants.map((restaurant) => (
+              <option key={restaurant.id} value={restaurant.id}>
+                {restaurant.name}
+              </option>
+            ))}
+          </select>{" "}
+          sell?
+        </h2>
+        {pizzasSoldBySelectedRestaurant.map((pizza) => (
+          <li key={pizza.id}>{pizza.name}</li>
         ))}
       </ul>
       <h2>
