112676fcc3753dd2d9c23d1ee56a184ca267dfe5
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-03-16 18:44:28 +0100

    added a select field to the restaurantlist component

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,10 +1,14 @@
 import { useSelector } from "react-redux";
 //import { selectRestaurants } from "../store/restaurants/selectors";
 import { selectRestaurantsWithPizzas } from "../store/selectors";
+import { selectPizzas } from "../store/pizzas/selectors";
+import { useState } from "react";
 
 export default function RestaurantList() {
   //const restaurants = useSelector(selectRestaurants);
   const restaurants = useSelector(selectRestaurantsWithPizzas);
+  const pizzas = useSelector(selectPizzas);
+  const [selectedPizza, setSelectedPizza] = useState(pizzas[0].id);
 
   return (
     <div>
@@ -21,6 +25,22 @@ export default function RestaurantList() {
           </li>
         ))}
       </ul>
+      <h2>
+        Who sells{" "}
+        <select
+          value={selectedPizza}
+          onChange={(e) => {
+            setSelectedPizza(parseInt(e.target.value));
+          }}
+        >
+          {pizzas.map((pizza) => (
+            <option key={pizza.id} value={pizza.id}>
+              {pizza.name}
+            </option>
+          ))}
+        </select>{" "}
+        ?
+      </h2>
     </div>
   );
 }
